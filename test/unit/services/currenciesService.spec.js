7023487042877876106ee549d49628b49cca17fc
student <michal.niedziela@labnoratory.com>
2017-02-13 13:26:49 +0100

    change test

--- test/unit/services/currenciesService.spec.js
+++ test/unit/services/currenciesService.spec.js
@@ -5,7 +5,6 @@ describe('CurrenciesService', function ()
     var CurrenciesMock;
     var httpMock;
     var qMock;
-    var httpBackend;
     var fakeRates = [
         {
             buy: 4.1635, code: 'USD', date: '2017-01-20', sell: 4.0811
@@ -15,10 +14,8 @@ describe('CurrenciesService', function ()
             buy: 5.123, code: 'GBP', date: '2017-01-20', sell: 5.0216
         }
     ];
-    var urls;
-    var urlsCalls = [];
-    var count = 0;
     var rootScope;
+    var result;
 
     beforeEach(module('cinkciarzTraining', function ($provide)
     {
@@ -29,30 +26,21 @@ describe('CurrenciesService', function ()
             })
         });
     }));
-    beforeEach(inject(function (_$httpBackend_,_CurrenciesService_, _$http_, _$q_, _$rootScope_)
+    beforeEach(module('cinkciarzTraining', function ($provide)
     {
-        httpBackend = _$httpBackend_;
+        httpMock = jasmine.createSpyObj('http', ['get']);
+        httpMock.get.and.returnValue(successfulPromise({data: fakeRates[0]}));
+        $provide.value('$http', httpMock);
+
+    }));
+
+    beforeEach(inject(function (_CurrenciesService_, _$q_, _$rootScope_)
+    {
         qMock = _$q_;
-        httpMock =_$http_;
         CurrenciesMock = _CurrenciesService_;
         rootScope = _$rootScope_;
 
-        /*urls = [{url: 'https://api.nbp.pl/api/exchangerates/rates/c/usd/today/'}, {url: 'https://api.nbp.pl/api/exchangerates/rates/c/eur/today/'},
-            {url: 'https://api.nbp.pl/api/exchangerates/rates/c/gbp/today/'}];
-        count = urls.length;
-        angular.forEach(urls, function (url)
-        {
-            urlsCalls.push(httpMock.get(url.url));
-        });*/
-
-
-
-
     }));
-    afterEach(function() {
-    // httpBackend.verifyNoOutstandingExpectation();
-    // httpBackend.verifyNoOutstandingRequest();
-    });
 
     describe('getCurencies', function ()
     {
@@ -60,19 +48,17 @@ describe('CurrenciesService', function ()
         {
             beforeEach(function ()
             {
-                // jasmine.createSpy(CurrenciesMock, 'getCurrencies').and.callFake(
-                //         function(){ return successfulPromise(fakeRates);});
-            });
-            it('should return rates', function (done)
-            {
-                var promise = CurrenciesMock.getCurrencies();
-                promise.then(function (response)
+                CurrenciesMock.getCurrencies().then(function (data)
                 {
-
-                    expect(response).toBe(fakeRates);
+                    result = data;
                 });
-                done();
+                rootScope.$apply();
 
+            });
+            it('should return rates', function ()
+            {
+
+                expect(result).toEqual(fakeRates);
             });
         });
     });
