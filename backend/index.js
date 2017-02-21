53b3169f612154b627fd4e73ffbcd9701dae3f7c
student <michal.niedziela@labnoratory.com>
2017-02-21 15:26:31 +0100

    add postgresql image, initialize database

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,36 @@ const Hapi = require('hapi');
 const config = require('./config');
 const routes = require('./rest/routes');
 const plugins = require('./plugins/plugins');
+const Pool = require('pg').Pool;
+const configPostgres = require('./postgres_config');
+
+process.on('unhandledRejection', function (e)
+{
+    console.log(e.message, e.stack);
+});
+
+var pool = new Pool(configPostgres);
 
 
 module.exports = function ()
 {
     const server = new Hapi.Server();
     server.connection({
-        host: 'localhost',
-        port: config.port
+        host: 'localhost', port: config.port
     });
     plugins(server);
     routes(server);
-    server.start((err) => {
+    let sql = 'CREATE TABLE IF NOT EXISTS wallet (id SERIAL PRIMARY KEY,PLN BIGINT,EUR BIGINT,GBP BIGINT,USD BIGINT )';
+    pool.query(sql)
+            .then(() =>
+    {
+        server.start((err) =>
+        {
 
-        if (err) {
-            throw err;
-        }
-        console.log('Server running at:', server.info.uri);
-});
+            if (err) {
+                throw err;
+            }
+            console.log('Server running at:', server.info.uri);
+        });
+    });
 };
